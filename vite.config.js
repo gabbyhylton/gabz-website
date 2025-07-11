378879e0c4cdcd5543d73626ffd3a8a2ea7e979e
Gabriella (Gabby) Hylton <gabriella.hylton@gmail.com>
2025-07-11 16:14:53 -0400

    fixed!!
    deploy needed to be "dist" instead of "build" bc its vite and not react

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "gabz-website.github.io",
+  base: "/gabz-website.github.io/",
 })
