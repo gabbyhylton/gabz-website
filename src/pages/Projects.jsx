080d2cedb68156ff061feb8b1d3bb32adee2969d
Gabriella (Gabby) Hylton <gabriella.hylton@gmail.com>
2026-02-06 16:32:42 -0500

    nuplanner project
    fixed mouse hover, now whenever a user hovers their mouse over the project image, it will show the projects description. need to be more descriptive with the project description.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,21 +2,29 @@ import { useState } from 'react';
 import plannerdemo from '../assets/nuplannerdemo_TEMP.png';
 
 export default function Projects() {
-    const [showDescription1, setShowDescription1] = useState(false);
+    const [hovered, setHovered] = useState(false);
     return (
         <div className="projects">
             <h1>Projects</h1>
-            { showDescription1 ? (
-                <p className='plannerdesc'
-                onClick={() => setShowDescription1(false)}
-                style={{cursor: "pointer"}}>
-                    planner description
-                </p>
-            ) : <img src={plannerdemo} onClick={() => setShowDescription1(!showDescription1)}
-            alt="Planner" 
-            style= {{ width: "500px", height: "500px", objectFit: "cover" }}
-            />}
+                <div className="project-card"
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                    // style={{cursor: "pointer"}}
+                >
+                    <img src={plannerdemo}
+                        alt="Planner"
+                        className={`project-image ${hovered ? "hidden" : ""}`}
+                        /> 
+                    <p className={`plannerdesc ${hovered ? "visible" : ""}`}>
+                        A Java-based scheduling application that allows users to view their weekly schedule.
+                        The scheduler uses 24-hour time, and highlights the time block in which the current user has an event.
+                        Additionally, the scheduler can parse XML schedule files while conversely creating XML files from
+                        a registered schedule.
+                    </p>
+
+                </div>
             <p>Coming soon!!</p>
         </div>
+        
     )
 }
