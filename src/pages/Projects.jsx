8df6f4344b106c08c3d59d49aaed572f915b5ee8
Gabriella (Gabby) Hylton <gabriella.hylton@gmail.com>
2026-02-06 15:38:50 -0500

    updating projects page
    adding projects to project page. clickable page to see project contents. switching to mouse hover feature.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import plannerdemo from '../assets/nuplannerdemo_TEMP.png';
+
 export default function Projects() {
+    const [showDescription1, setShowDescription1] = useState(false);
     return (
-        <div className="page">
+        <div className="projects">
             <h1>Projects</h1>
+            { showDescription1 ? (
+                <p className='plannerdesc'
+                onClick={() => setShowDescription1(false)}
+                style={{cursor: "pointer"}}>
+                    planner description
+                </p>
+            ) : <img src={plannerdemo} onClick={() => setShowDescription1(!showDescription1)}
+            alt="Planner" 
+            style= {{ width: "500px", height: "500px", objectFit: "cover" }}
+            />}
             <p>Coming soon!!</p>
         </div>
     )
